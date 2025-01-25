0f1efca375b6aa189fa1adb48977d8da8d22b488
monolithdragon <joo.lajos76@gmail.com>
2025-01-25 17:22:47 +0100

    Implement interaction

--- index.js
+++ index.js
@@ -1,6 +1,116 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'~',
+	'`',
+	'!',
+	'@',
+	'#',
+	'$',
+	'%',
+	'^',
+	'&',
+	'*',
+	'(',
+	')',
+	'_',
+	'-',
+	'+',
+	'=',
+	'{',
+	'[',
+	'}',
+	']',
+	',',
+	'|',
+	':',
+	';',
+	'<',
+	'>',
+	'.',
+	'?',
+	'/',
+];
 
+const passwordFirst = document.getElementById('password-01');
+const passwordSecond = document.getElementById('password-02');
+const passwordLength = 15;
 
+function generatePassword() {
+	ResetContent();
 
+	for (let index = 0; index < passwordLength; index++) {
+		const characterIndex = Math.floor(Math.random() * characters.length);
 
+		passwordFirst.textContent += characters[characterIndex];
+		passwordSecond.textContent +=
+			characters[
+				characterIndex === 0 ? characterIndex + 1 : characterIndex - 1
+			];
+	}
+}
+
+function ResetContent() {
+	passwordFirst.textContent = '';
+	passwordSecond.textContent = '';
+}
